61b4b9e2f1ae85f22ede7d3dada371d6a9122113
zaman-pro <mdalimzaman.23@gmail.com>
2025-03-06 22:29:32 +0600

    destructure

---
+++ destructure.js
@@ -0,0 +1,24 @@
+// const newObj = {
+//   name: "zaman",
+//   age: 25,
+//   friends: ["abul", "babul", "dabul", "kabul"],
+//   status: "single",
+// };
+
+// const { name, age, friends, status } = newObj;
+
+// console.log(name, age, status);
+
+// const newArr = ["zero", "one", "two", "three"];
+
+// const [initI, oneI, twoI, threeI] = newArr;
+
+// console.log(threeI);
+
+const employee = {
+  firstName: "jump",
+  lastName: "tobacco",
+};
+
+delete employee.firstName;
+console.log(employee.lastName);
---
+++ spread-operator.js
@@ -0,0 +1,9 @@
+// const newMembers = ["ali", "bali", "cali", "dali"];
+
+// const moreNewMembers = [...newMembers, "elie", "fali", "gali"];
+
+// console.log(...moreNewMembers);
+
+const numbers = [23, 26, 63, 76, 64, 1, 5, 7, 8, 5];
+
+console.log(Math.max(...numbers));
